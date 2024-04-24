ddcf7224712e3d2d267810a85fdac589d72aaed6
Brando123412 <130480267+Brando123412@users.noreply.github.com>
2024-04-24 17:09:05 -0500

    brando.1

---
+++ Assets/ResourceGame/Scripts/IA/Actions/AntionNodeActions.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime.Tasks;
+
+[TaskCategory("IA SC/Node Base")]
+public class AntionNodeActions : ActionNode
+{
+    public override void OnStart()
+    {
+        base.OnStart();
+    }
+}
