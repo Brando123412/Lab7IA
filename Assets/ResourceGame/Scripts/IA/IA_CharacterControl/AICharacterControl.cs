3e57ace84b62acd3cb125e1aa6444a0988e2e8a3
IvanCacereZ <130480223+IvanCacereZ@users.noreply.github.com>
2024-04-24 16:51:00 -0500

    ivan

---
+++ Assets/ResourceGame/Scripts/IA/IA_CharacterControl/AICharacterControl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using BehaviorDesigner.Runtime;
+
+[RequireComponent(typeof(BehaviorTree))]
+public class AICharacterControl : MonoBehaviour
+{
+    
+}
