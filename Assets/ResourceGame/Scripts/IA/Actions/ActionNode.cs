d6aa9dae8fa8341b7f06abbe60fa2a876ebecbae
Brando123412 <130480267+Brando123412@users.noreply.github.com>
2024-04-24 16:50:57 -0500

    Brando

---
+++ Assets/ResourceGame/Scripts/IA/Actions/ActionNode.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using BehaviorDesigner.Runtime.Tasks;
+
+[TaskCategory ("IA SC/Node Base")]
+
+public class ActionNode : Action
+{
+    protected AICharacterVehicle _AICharacterVehicle;
+    protected AICharacterAction _AICharacterAction;
+    //protected TypeUniy _TypeUniy;
+    public override void OnStart()
+    {
+        _AICharacterVehicle = GetComponent<AICharacterVehicle>();
+        if (_AICharacterVehicle == null)
+        {
+            Debug.LogWarning("Not load component");
+        }
+        _AICharacterAction = GetComponent<_AICharacterAction>();
+        if (_AICharacterAction == null)
+        {
+            Debug.LogWarning("Not load component");
+        }
+
+        if (_AICharacterVehicle == null)
+        {
+            Debug.LogWarning("Not load component");
+        }
+        base.OnStart();
+    }
+}
